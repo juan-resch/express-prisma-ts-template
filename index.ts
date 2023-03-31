a479d7991060640e1ad7550489bedded9f7c4aca
Juan Resch <55303897+juan-resch@users.noreply.github.com>
2023-03-31 16:51:51 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import morgan from 'morgan'
+import morgan from 'morgan';
 import helloRoutes from 'routes/hello.routes';
-
+import cors from 'cors';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(
-  morgan("📥 [server]: :method :status :url :res[content-length] - :response-time ms")
-);
+app.use(cors);
+app.use(express.json());
+app.use(morgan('📥 [server]: :method :status :url :res[content-length] - :response-time ms'));
 
 app.use('/hello', helloRoutes);
 
